854b78014583951aa89159b8b9fa22c6ff382a17
rsucasasf <roi.sucasas@atos.net>
2017-05-25 11:44:03 +0200

    tests updated

--- test/cloj_rules_engine/rules_mng_test.clj
+++ test/cloj_rules_engine/rules_mng_test.clj
@@ -3,8 +3,12 @@
             [cloj-rules-engine.rules-mng :refer :all]))
 
 (deftest a-test
+  (testing "It's ok."
+    (is (= 1 1))))
+
+(deftest b-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
 
-(run-tests 'cloj-rules-engine.rules-mng-test)
+;(run-tests 'cloj-rules-engine.rules-mng-test)
 ;(run-all-tests)
