0bb84e597e6a19b3d01529d5dd3ee783e2274d6b
rsucasasf <roi.sucasas@atos.net>
2017-05-26 11:40:56 +0200

    tests updated

--- test/cloj_rules_engine/rules_mng_test.clj
+++ test/cloj_rules_engine/rules_mng_test.clj
@@ -2,13 +2,11 @@
   (:require [clojure.test :refer :all]
             [cloj-rules-engine.rules-mng :refer :all]))
 
-(deftest a-test
-  (testing "It's ok."
-    (is (= 1 1))))
+(deftest test-01
+  (testing "It's ok." (is (= 1 1))))
 
-;(deftest b-test
-;  (testing "FIXME, I fail."
-;    (is (= 0 1))))
+(deftest test-02
+  (testing "Initialize rules" (is (initialize "rules.clj"))))
 
 ;(run-tests 'cloj-rules-engine.rules-mng-test)
 ;(run-all-tests)
