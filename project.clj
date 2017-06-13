692ea215a51ac03a3874cabe1cfaa5322dc54583
rsucasasf <rsucasas@gmail.com>
2017-06-13 17:07:57 +0200

    project.clj updated to version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cloj-rules-engine "0.1.1"
+(defproject cloj-rules-engine "0.1.2-SNAPSHOT"
   :description "a very simple rules engine written in Clojure"
   :url "https://github.com/rsucasasf/cloj-rules-engine"
   :license {:name "Eclipse Public License"
@@ -14,12 +14,17 @@
                  [org.clojure/data.json "0.2.6"]          ; Eclipse Public License - Version 1.0  https://github.com/clojure/data.json
                  [proto-repl "0.3.1"]]                    ; MIT                                   https://github.com/jasongilman/proto-repl
   :target-path "target/%s"
-  :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]]            ; MIT                                   https://github.com/codecov/example-clojure
+  :plugins [[camechis/deploy-uberjar "0.3.0"]
+            [lein-cloverage "1.0.7-SNAPSHOT"]]            ; MIT                                   https://github.com/codecov/example-clojure
   :profiles {
     :uberjar {:aot :all}
     :dev {:resource-paths ["resources"]
     ;; jvm configuration
     :jvm-opts ["-Xmx256M"]}
+
+  :lein-release {:deploy-via :clojars :build-uberjar true}
+
+
   ;; deploy-repositories
   ;:deploy-repositories {"releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/" :creds :gpg}
   ;                      "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/" :creds :gpg}}
